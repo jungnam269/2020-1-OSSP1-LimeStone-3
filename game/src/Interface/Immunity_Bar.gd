f95464b3b4cf1f01fe05395ae68238c6b2386330
JungEunbee <eunbeejung97@gmail.com>
2020-05-24 23:56:33 +0900

    reset

--- game/src/Interface/Immunity_Bar.gd
+++
@@ -1,5 +0,0 @@
-extends HBoxContainer
-
-func _on_Interface_immunity_changed(immunity):
-	$TextureProgress.value = immunity
-##아직 수정 중
--- game/src/Interface/Interface.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-signal immunity_changed(immunity)
-signal enemmys_changed(count)
-
-##아직 수정 중
-#Player에 면역 수치(hp같이) 추가해주어야함

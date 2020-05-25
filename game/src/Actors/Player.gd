67be4317caba4f7f76ae64f1748581ff50f0d7cc
shown45 <45488388+shown45@users.noreply.github.com>
2020-05-25 21:14:06 +0900

    Player and enemy Logic updated
    Player and enemy Logic updated

--- game/src/Actors/Player.gd
+++ game/src/Actors/Player.gd
@@ -1,54 +1,34 @@
 extends Actor
 
+var laser = preload("res://src/Attack/LaserBeam.tscn")
 
-export var stomp_impulse: = 600.0
+func _process(delta):	#스프라이트 적용과 버튼 입력에 따라 스프라이트를 좌우 반전 시킴
+	
+	if Input.is_action_pressed("move_right"):
+		$AnimatedSprite.play("move")
+		$AnimatedSprite.flip_h = false
+	elif Input.is_action_pressed("move_left"):
+		$AnimatedSprite.play("move")
+		$AnimatedSprite.flip_h = true
+	else:
+		$AnimatedSprite.stop()
+	
+	if Input.is_action_pressed("attack") && get_node("LaserBeam") == null:
+		var laser_shoot_instance = laser.instance()
+		add_child(laser_shoot_instance)
+		laser_shoot_instance.position = $Eyezone.position
+		
+	
+	if Input.is_action_just_released("attack"):
+		get_node("LaserBeam").queue_free()
 
-
-func _on_StompDetector_area_entered(area: Area2D) -> void:
-	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
-
-
-func _on_EnemyDetector_body_entered(body: PhysicsBody2D) -> void:
-	die()
-
-
-func _physics_process(delta: float) -> void:
-	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
+func _physics_process(delta: float) -> void: #움직이는 물리 구현
 	var direction: = get_direction()
-	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
-	var snap: Vector2 = Vector2.DOWN * 60.0 if direction.y == 0.0 else Vector2.ZERO
-	_velocity = move_and_slide_with_snap(
-		_velocity, snap, FLOOR_NORMAL, true
-	)
-
-
-func get_direction() -> Vector2:
+	velocity = speed * direction
+	velocity = move_and_slide(velocity)
+	
+func get_direction() -> Vector2: #입력을 통한 방향이동
 	return Vector2(
-		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-Input.get_action_strength("jump") if is_on_floor() and Input.is_action_just_pressed("jump") else 0.0
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 1.0
 	)
-
-
-func calculate_move_velocity(
-		linear_velocity: Vector2,
-		direction: Vector2,
-		speed: Vector2,
-		is_jump_interrupted: bool
-	) -> Vector2:
-	var velocity: = linear_velocity
-	velocity.x = speed.x * direction.x
-	if direction.y != 0.0:
-		velocity.y = speed.y * direction.y
-	if is_jump_interrupted:
-		velocity.y = 0.0
-	return velocity
-
-
-func calculate_stomp_velocity(linear_velocity: Vector2, stomp_impulse: float) -> Vector2:
-	var stomp_jump: = -speed.y if Input.is_action_pressed("jump") else -stomp_impulse
-	return Vector2(linear_velocity.x, stomp_jump)
-
-
-func die() -> void:
-	PlayerData.deaths += 1
-	queue_free()
+	

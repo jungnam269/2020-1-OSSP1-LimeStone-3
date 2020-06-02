e692059fa46f88d5a5b2b7b8e3a542f359e39d37
Seyoung1220 <62585390+Seyoung1220@users.noreply.github.com>
2020-06-02 12:49:36 +0900

    Add files via upload

---
+++ game/src/UserInterface/EndingButton2.gd
@@ -0,0 +1,7 @@
+#두번째 엔딩 결과 화면
+extends Button
+
+
+
+func _on_EndingButton2_button_up() :
+	get_tree().quit()
---
+++ game/src/UserInterface/EndingButton3.gd
@@ -0,0 +1,7 @@
+#세번째 엔딩 결과 화면
+extends Button
+
+
+
+func _on_EndingButton3_button_up() :
+	get_tree().quit()

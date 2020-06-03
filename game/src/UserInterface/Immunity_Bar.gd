9feec27c86c980095b52a9e84da7e6b678063b03
shown45 <45488388+shown45@users.noreply.github.com>
2020-06-04 03:45:27 +0900

    Uploaded health bar and bgm
    Let the game begin

--- game/src/UserInterface/Immunity_Bar.gd
+++ game/src/UserInterface/Immunity_Bar.gd
@@ -1,5 +1,8 @@
 extends HBoxContainer
 
-func _on_Interface_immunity_changed(immunity):
-	$TextureProgress.value = immunity
+#func _on_Interface_immunity_changed(immunity):
+#	$TextureProgress.value = immunity
 ##아직 수정 중
+func _on_Player_immunedamage(hp):
+	var Health = hp
+	

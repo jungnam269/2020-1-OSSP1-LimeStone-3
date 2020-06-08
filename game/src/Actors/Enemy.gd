6a3b85a77f311d66bde27a91cf06d3b9b65b00d9
JungEunbee <eunbeejung97@gmail.com>
2020-06-09 06:22:03 +0900

    Modified Enemy.gd

--- game/src/Actors/Enemy.gd
+++ game/src/Actors/Enemy.gd
@@ -1,12 +1,17 @@
 extends "res://src/Actors/Actor.gd"
  #적 구현 예시 1. 플레이어를 쫒아오는 
 
+onready var hpbar = get_node("HPbar")
+
 var hp = 100
+var current_hp
+var damage = 20
 signal mouseon
 signal mouseoff
 
 func _ready() -> void:	#적은 플레이어 시야에 출현하기 전에는 작동하지 않는다.
 	set_physics_process(false)
+	current_hp=hp
 	
 func _physics_process(delta: float) -> void:	#화면에 출현한 적은 플레이어쪽으로 일정한 속도로 움직
 	var direction = (get_node('../Player').position - position).normalized()
@@ -27,16 +32,25 @@ func _process(delta): #기본 상태에서는 stop 에 해당하는 스프라이
 		
 func _on_Area2D_area_entered(area):
 	print("Oh!")
+	Damaged(damage)
 	set_process(false)
 	set_physics_process(false)
 	$AnimatedSprite.play("zapp")
 	$AudioStreamPlayer2D.play()
 	
-func _on_Area2D_area_exited(area):
+func _on_Area2D_area_exited(area): 
 	print("Oh!2")
 	set_process(true)
 	set_physics_process(true)
-	die()
+	if current_hp <= 0: #마지막 공격을 받고나서 리소스 출력하고 죽음
+		die()
 	
+func Damaged(damage): #적이 공격받을 때 hp감소 & 시각화
+	current_hp -= damage
+	get_node("HPbar").value=int(current_hp)
+
+		
 func die():
+	#죽을때 animation
 	queue_free()
+	hpbar.hide()

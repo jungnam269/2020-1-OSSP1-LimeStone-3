7861afe25189f7761aa5a7bf5c50d4b625363483
jungnam269 <jungnam269@gmail.com>
2020-06-15 11:13:30 +0900

    ADD pause in stage1

--- game/src/Countdown_timer/PauseMenuLayer.gd
+++ game/src/Countdown_timer/PauseMenuLayer.gd
@@ -4,21 +4,13 @@ extends Control
 func _ready():
 
 	hide()
-	
+
 
 func _input(event):
 	if event.is_action_pressed("pause"):
 		var new_pause_state = not get_tree().paused
 		get_tree().paused = new_pause_state
 		visible = new_pause_state
-
-
-func _on_Pausegameinbtn_pressed():
-	print("Button Pressed")
-	var new_pause_state = not get_tree().paused
-	get_tree().paused = new_pause_state
-	visible = new_pause_state
-	
 
 func _on_Resumebtn_pressed():
 	print("Resume Button Pressed")
@@ -34,3 +26,10 @@ func _on_Exitbtn_button_up():
 func _on_Restartbtn_button_up():
 	get_tree().change_scene("res://src/Countdown_timer/RestartAsk.tscn")
 
+
+
+func _on_PauseBtn_inS1_pressed():
+	print("Button Pressed")
+	var new_pause_state = not get_tree().paused
+	get_tree().paused = new_pause_state
+	visible = new_pause_state

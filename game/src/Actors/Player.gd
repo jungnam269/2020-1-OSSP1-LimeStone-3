17a44ac8f605e310d4adb8b1709c324c9b601553
shown45 <45488388+shown45@users.noreply.github.com>
2020-06-16 13:47:30 +0900

    new attack ahd health bar
    as i said

--- game/src/Actors/Player.gd
+++ game/src/Actors/Player.gd
@@ -6,12 +6,21 @@ export var fevermode = false
 var laser = preload("res://src/Attack/LaserBeam.tscn")
 var laser2 = preload("res://src/Attack/LaserBeam2.tscn")
 var facingRight = true
+var isattack = false
+var damage = 20
+var attackdamage = 50
 
 func _ready():
 	emit_signal("immunedamage",immunity)
 
 func _process(delta):	#스프라이트 적용과 버튼 입력에 따라 스프라이트를 좌우 반전 시킴
-	
+	normalphysics(delta)
+	if isattack :
+		Damaged(damage*delta)
+	if immunity >= 100 :
+		fevermode = true
+
+func normalphysics(delta):
 	if not facingRight:
 		$Eyezone.position.x = -$Eyezone.position.x
 	
@@ -51,13 +60,18 @@ func get_direction() -> Vector2: #입력을 통한 방향이동
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 1.0
 	)
+
+func Damaged(damage): #적이 공격받을 때 hp감소 & 시각화
+	immunity -= damage
+	get_node("Camera2D2/Interface/TextureProgress").value=int(immunity)
 	
 func _on_Immune_area_entered(area):
-	immunity -= 10
+	isattack = true
 	$AnimatedSprite.set_modulate(Color.red)
 	print("checking %s", immunity) #test
 	emit_signal("immunedamage",immunity)
 
 
 func _on_Immune_area_exited(area):
+	isattack = false
 	$AnimatedSprite.set_modulate(Color.white)

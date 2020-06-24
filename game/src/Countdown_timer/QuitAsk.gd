d21be90b156d30f02c32ecca84b8d9d7aff0616f
jungnam269 <jungnam269@gmail.com>
2020-06-24 22:07:50 +0900

    add interface tscn in level2,3

--- game/src/Countdown_timer/QuitAsk.gd
+++ game/src/Countdown_timer/QuitAsk.gd
@@ -9,4 +9,4 @@ func _on_QuitAskbtY_button_up():
 	get_tree().quit()
 
 func _on_QuitAskbtN_button_up():
-	get_tree().change_scene("")#원래씬이라 연결
+	get_tree().change_scene("res://src/Levels/Stage1.tscn")#원래씬이라 연결
